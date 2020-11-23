273e67d2d4742413894c9f6f50c5c9f3b903cbdc
Nour Gaser <nourgaser2012@gmail.com>
2020-11-23 11:12:55 +0200

    First commit

---
+++ src/main/java/main/Main.java
@@ -0,0 +1,20 @@
+package main;
+
+import java.awt.Color;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author GPC
+ */
+public class Main {
+    public static void main(String args[]) {
+        Frame myFrame = new Frame();
+        myFrame.main(null);
+    }
+}
